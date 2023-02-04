201983af5fb7157442a39dbb7c46be7bf5453137
bwj <648507447@qq.com>
2023-02-04 17:36:03 +0800

    统计分析功能基本完成

--- routes/analysis.py
+++ routes/analysis.py
@@ -1,18 +1,13 @@
 from flask import Flask, request, jsonify, Blueprint
-from analysis.analysis_base import area, trend, author
+from analysis.analysis_base import analyze_by_list
 
 analysis_bp = Blueprint('analysis_bp', __name__)
 
 @analysis_bp.route('/analysis', methods=['POST'])
-def pantent_analysis():
+def patent_analysis():
     if request.method == 'POST':
         data = request.json
         anaType = data.get('anaType')
         figType = data.get('figType')
         patentIds = data.get('patentIds')
-        if anaType == 'area':
-            return jsonify(area(patentIds, figType))
-        elif anaType == 'trend':
-            return jsonify(trend(patentIds, figType))
-        elif anaType == 'author':
-            return jsonify(author(patentIds, figType))
+        return jsonify(analyze_by_list(patentIds, figType, anaType))
