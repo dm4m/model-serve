963ca9fde2c22eb26f0ec56429c0d945fd7451d5
bwj <648507447@qq.com>
2023-02-18 20:02:53 +0800

    统计分析功能基本完成

--- repository/milvus_source.py
+++ repository/milvus_source.py
@@ -5,7 +5,7 @@ def get_relevant_vec_results(collection_name, field, query, limit = 100, output_
     connections.connect(alias="default", host='localhost', port='19530')
     collection = Collection(collection_name)  # Get an existing collection.
     collection.load()
-    pt_inputs = tokenizer(query, return_tensors="pt").to(device)
+    pt_inputs = tokenizer(query, return_tensors="pt", max_length=510, truncation=True).to(device)
     pt_outputs = model(**pt_inputs)
     query_embeddings = [pt_outputs["last_hidden_state"][0][0].tolist()]
     print("query vector:" + str(query_embeddings))
