ad5a73c3895f57e19ae459074162a96ba134be94
bwj <648507447@qq.com>
2023-05-10 14:22:37 +0800

    新颖性分析结果，权利要求相似性分数

--- services/novelty_service.py
+++ services/novelty_service.py
@@ -1,3 +1,4 @@
+from . import *
 import re
 from novelty.compare import novelty_compare
 from repository.milvus_source import get_relevant_id_list
@@ -51,7 +52,7 @@ def signory_analysis(signory_items):
 
 def signory_item_analysis(signory_item):
     # 1.recall relevant signory items 2. extract and compare
-    relevant_signory_ids, scores = get_relevant_id_list("signory", "signory", signory_item, limit=5)
+    relevant_signory_ids, distances = get_relevant_id_list("signory", "signory", signory_item, limit=5)
     relevant_signorys = get_sig_by_id(relevant_signory_ids)
     ans_result = []
     for index, sig in enumerate(relevant_signorys) :
@@ -61,7 +62,7 @@ def signory_item_analysis(signory_item):
         tmp['relevant_sig'] = sig["signory_seg"]
         tmp['ori_patent_title'] = sig["title"]
         tmp['compare_result'], tmp['statistical_dict'], tmp['statistical_info'] = novelty_compare(signory_item, sig["signory_seg"])
-        tmp['score'] = scores[index]
+        tmp['score'] = getscore(distances[index])
         ans_result.append(tmp)
     return ans_result
 
