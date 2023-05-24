f8805692d465188e3cf564742a63f3c6cc8e36cd
Alison-starbeat <2898117954@qq.com>
2023-05-24 14:59:17 +0800

    添加了两种检索方式，通过标题+主权项加权的混合检索方式；或者通过标题+全部权利要求加权的检索方式

--- services/search_service.py
+++ services/search_service.py
@@ -25,16 +25,28 @@ def patent_neural_search(field, query,schema=None):
         sig_id_list = []
         id_list = []
         embed_list = []
-        signory_id = FieldSchema(name="signory_id", dtype=DataType.INT64, is_primary=True,auto_id=False,description="")
-        patent_id = FieldSchema(name="patent_id", dtype=DataType.INT64,description="")
-        signory = FieldSchema(name="signory", dtype=DataType.FLOAT_VECTOR,dim=768,description="")
-        schema = CollectionSchema(fields=[signory_id, patent_id, signory],auto_id=False,description="signory_seg of patent,HNSW")
+        # signory_id = FieldSchema(name="signory_id", dtype=DataType.INT64, is_primary=True,auto_id=False,description="")
+        # patent_id = FieldSchema(name="patent_id", dtype=DataType.INT64,description="")
+        # signory = FieldSchema(name="signory", dtype=DataType.FLOAT_VECTOR,dim=768,description="")
+        # schema = CollectionSchema(fields=[signory_id, patent_id, signory],auto_id=False,description="signory_seg of patent,HNSW")
         query_embeddings = encode(query)
-        id_list,embed_list,sig_id_list = get_relevant_all_field_results("signory", "signory", schema, query_embeddings, limit = 100, output_list = ["signory_id","patent_id"])
+        id_list,embed_list,sig_id_list = get_relevant_all_field_results("signory", "signory", schema, query_embeddings, limit = 100, output_list = ["signory_id","patent_id"],out_type="sig")
         return_list = []
         return_list.append(id_list)
         return_list.append(embed_list)
         return_list.append(sig_id_list)
+        return return_list
+    elif field == "titleField":
+        id_list = []
+        title_e_list = []
+        query_embeddings = encode(query)
+        id_list, title_e_list = get_relevant_all_field_results("title", "title", schema,
+                                                                          query_embeddings, limit=100,
+                                                                          output_list=["id"],out_type="title")
+        return_list = []
+        return_list.append(id_list)
+        return_list.append(title_e_list)
+        return_list.append(id_list)
         return return_list
 
 
@@ -63,3 +75,45 @@ def search_by_patent(signory_list):
     # 增加返回值分数，该分数为rrf算法得到的聚合向量的分数，值越大越相似，没有区间限制
     rerank_list,rerank_score = rerank(last_query_embedding,last_other_embedding)
     return rerank_list,rerank_score
+
+def search_by_title_sig(title,signory_list,search_type):
+    # search_type = “first_sig”(就是标题+主权项),其他的就是(标题+所有权利要求)
+    first_signory_weight = 0.6
+    other_signory_weight = 0.3
+    title_weight = 1
+    # 写支持title的方式
+    all_embed = []
+    t_embedding = encode(title)
+    all_embed.append(t_embedding[0])
+
+    id_ = FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=False)
+    title_ = FieldSchema(name="title", dtype=DataType.FLOAT_VECTOR, dim=768)
+    schema = CollectionSchema(fields=[id_, title_], description="title of patent,HNSW")
+    t_list= patent_neural_search("titleField", title,schema)
+    rawrank = []
+    # title:[id,embedding]
+    rawrank.append(t_list)
+
+    signory_id = FieldSchema(name="signory_id", dtype=DataType.INT64, is_primary=True, auto_id=False, description="")
+    patent_id = FieldSchema(name="patent_id", dtype=DataType.INT64, description="")
+    signory = FieldSchema(name="signory", dtype=DataType.FLOAT_VECTOR, dim=768, description="")
+    schema = CollectionSchema(fields=[signory_id, patent_id, signory], auto_id=False,
+                              description="signory_seg of patent,HNSW")
+    for i in range(0, len(signory_list)):
+        # print("1")
+        now_embedding = encode(signory_list[i])
+        all_field_list = patent_neural_search("allField", signory_list[i], schema)
+        rawrank.append(all_field_list)
+        all_embed.append(now_embedding[0])
+        if search_type == "first_sig":
+            break
+    last_query_embedding = torch.tensor([0.0 for i in range(0, 768)])
+    temp_sig = np.add.reduce(all_embed[2:])
+    last_query_embedding = title_weight * torch.tensor(all_embed[0]) + first_signory_weight * torch.tensor(all_embed[1]) + other_signory_weight * torch.tensor(
+        temp_sig)
+    last_other_embedding = aggregate(rawrank)
+    # 增加返回值分数，该分数为rrf算法得到的聚合向量的分数，值越大越相似，没有区间限制
+    rerank_list, rerank_score = rerank(last_query_embedding, last_other_embedding)
+    return rerank_list,rerank_score
+
+
